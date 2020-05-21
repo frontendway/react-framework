f19747dd74a276494407f9f788623b444cba2cbe
zhaom <shs-zhcsyfczyzm@chinaunicom.cn>
2020-05-21 16:31:29 +0800

    editor welcome page

--- src/features/examples/WelcomPage.jsx
+++ src/features/examples/WelcomPage.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 class WelcomePage extends React.Component {
   render() {
     return <div className="examples-WelcomePage">
-      WelcomePage
+      <h2>WelcomePage</h2>
     </div>
   }
 }
