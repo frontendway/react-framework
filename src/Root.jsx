3d2e5eb01e32b9618517be0903ebb61c62b1b8d8
zhaom <shs-zhcsyfczyzm@chinaunicom.cn>
2020-05-22 11:36:29 +0800

    editor Root.jsx

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Provider } from 'react-redux'
+import store from './store/store'
 import { HashRouter, Route, Switch } from 'react-router-dom'
 import { DefaultPage } from './features/home'
 import { Layout } from './features/examples'
@@ -7,13 +9,15 @@ import { NotFoundPage } from './features/common'
 class Root extends React.Component {
   render() {
     return (
-      <HashRouter>
+      <Provider store={store.getStore()}>
+        <HashRouter>
         <Switch>
-          <Route path="/" exact component={DefaultPage}></Route>
-          <Route path="/examples" component={Layout}></Route>
-          <Route path="*" component={NotFoundPage}></Route>
-        </Switch>
-      </HashRouter>
+            <Route path="/" exact component={DefaultPage}></Route>
+            <Route path="/examples" component={Layout}></Route>
+            <Route path="*" component={NotFoundPage}></Route>
+          </Switch>
+        </HashRouter>
+      </Provider>
     )
   }
 }
--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
 import Root from './Root'
-import store from './store/store'
 import './style/index.less'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store.getStore()}>
-      <Root />
-    </Provider>
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
